33dbc6d6b80d4cb7eef31b001efa40d52526f5c1
August <augustbberg@gmail.com>
2020-12-03 11:41:34 +0100

    Initial commit.

---
+++ R/drawFromMargin.R
@@ -0,0 +1,24 @@
+#' A pulpDistr function
+#'
+#' This function allows you to express your love of cats.
+#' @param marginGenerator Contains fitted univariate margin parameters
+#' @param quantilesToEval Contains quantiles to invert to obtain value
+#' @keywords cats
+#' @export
+#' @examples
+#' drawFromMargin()
+#'
+drawFromMargin <- function(marginGenerator,quantilesToEval){
+  if (marginGenerator$distname == "weibull"){
+    marginDraw = qweibull(quantilesToEval,marginGenerator$estimate[1],marginGenerator$estimate[2])
+  } else if (marginGenerator$distname == "gamma"){
+    marginDraw = qgamma(quantilesToEval, shape = marginGenerator$estimate[1], rate = marginGenerator$estimate[2])
+  } else if (marginGenerator$distname == "lnorm"){
+    marginDraw = qlnorm(quantilesToEval, meanlog = marginGenerator$estimate[1], sdlog = marginGenerator$estimate[2])
+  } else if (marginGenerator$distname == "norm"){
+    marginDraw = qnorm(quantilesToEval, mean = marginGenerator$estimate[1], sd = marginGenerator$estimate[2])
+  } else if (marginGenerator$distname == "exp"){
+    marginDraw = qexp(quantilesToEval, rate = marginGenerator$estimate[1])
+  }
+  return(marginDraw)
+}
