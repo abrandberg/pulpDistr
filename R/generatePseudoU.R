539419ae3f1ce0537226703033f478c0c38ce175
August <augustbberg@gmail.com>
2020-12-03 14:09:49 +0100

    Bugfix.

--- R/generatePseudoU.R
+++ R/generatePseudoU.R
@@ -2,7 +2,7 @@
 #'
 #' pseudoObs() takes as input a matrix or dataframe of observations
 #' and returns a matrix of ranked pseudo-observations
-#' [0, 1)^d where d is the number of columns in the input.
+#'  where d is the number of columns in the input.
 #'
 #'  The observations are normalized such that the value 1 is never
 #' returned, as this could generate some numerical difficulties
@@ -18,9 +18,9 @@
 pseudoObs <- function (observedSample){
   udata = matrix(0,dim(observedSample)[1],dim(observedSample)[2])
 
-  n<-length(pulp_df[,1])
-  for(i in 1:dim(pulp_df)[2] ){
-    udata[,i]<-rank(pulp_df[,i])/(n+1)
+  n<-length(observedSample[,1])
+  for(i in 1:dim(observedSample)[2] ){
+    udata[,i]<-rank(observedSample[,i])/(n+1)
   }
   return(pseudoObs)
 }
