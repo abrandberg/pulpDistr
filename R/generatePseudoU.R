aa2619d11aa58a0d937b6da9ae51cec784916bc5
August <augustbberg@gmail.com>
2020-12-03 12:27:10 +0100

    Documentation

--- R/generatePseudoU.R
+++ R/generatePseudoU.R
@@ -1,5 +1,12 @@
 #' A pulpDistr function
 #'
+#' pseudoObs() takes as input a matrix or dataframe of observations
+#' and returns a matrix of ranked pseudo-observations
+#' [0, 1)^d where d is the number of columns in the input.
+#'
+#'  The observations are normalized such that the value 1 is never
+#' returned, as this could generate some numerical difficulties
+#' later on.
 #'
 #' @param observedSample Matrix or dataframe of observations
 #' @keywords
