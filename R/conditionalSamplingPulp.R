f6d7ee347d2c390412fe8b7cbf437031cb5ffb42
August <augustbberg@gmail.com>
2020-12-03 15:55:21 +0100

    Bugfix

--- R/conditionalSamplingPulp.R
+++ R/conditionalSamplingPulp.R
@@ -12,7 +12,7 @@
 conditionalSamplingPulp <- function(saveDirName,pulp_cond,pulp_raw){
 
   idxToRetain = length(pulp_cond[,1])
-  conditioningCombined = do.call(cbind, list(pulp_cond, pulp_raw))
+  conditioningCombined = do.call(rbind, list(pulp_cond, pulp_raw))
 
   conditioningData = generatePseudoU(pulp_cond)
 
